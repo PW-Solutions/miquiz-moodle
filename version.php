672d92edcae4abac0cc7435d4108869fe4d65f82
nscbrandb <bernhard.brandstetter@fhstp.ac.at>
2018-04-10 08:50:34 +0200

    version bump

--- version.php
+++ version.php
@@ -8,7 +8,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_miquiz'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 20180040502;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 20180041002;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2010112400;  // Requires Moodle 2.0.
 $plugin->release   = '2017-08-06';
 $plugin->maturity = MATURITY_BETA;
