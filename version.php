d4de6c5b4280019dbc244619425eaf8e1365bcf8
Friedrich Pawelka <friedrich.pawelka@googlemail.com>
2026-04-14 00:46:44 +0200

    Bump version

--- version.php
+++ version.php
@@ -2,13 +2,13 @@
 
 /**
  * @package mod_miquiz
- * @author  2017-2023, Pawelka Wollmann Solutions GbR <info@pw-solutions.de>
+ * @author  2017-2026, Pawelka Wollmann Solutions GbR <info@pw-solutions.de>
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_miquiz'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2023092700;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041900;  // Requires Moodle 4.0.0.
-$plugin->release   = 'v4';
+$plugin->version   = 2026041400;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022112800;  // Requires Moodle 4.1.
+$plugin->release   = 'v4.4';
 $plugin->maturity = MATURITY_BETA;
